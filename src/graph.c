303b0bf059fdedf2246eac204352a211594906d7
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-07-15 10:38:58 +0800

    feat: add functions related to graph representation

--- src/graph.c
+++ src/graph.c
@@ -2,6 +2,9 @@
 // create gds here
 // linked list
 #include <string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define MAX_ID_LEN 8
 
 typedef struct ALIST_VERTEX{
@@ -9,18 +12,104 @@ typedef struct ALIST_VERTEX{
     struct ALIST_VERTEX *next;
 } *pVertex;
 
+/**
+ * list is the array of adj vertices of the head
+ */
+typedef struct HEAD_VERTEX{
+    char name[MAX_ID_LEN+1];
+    pVertex list;
+    struct HEAD_VERTEX *nextHead;
+}*pHead;
 typedef struct UDGRAPH{
     int nV;
-    pVertex *list;
+    pHead heads;
 } *pGraph;
 
-pVertex addVertex(char* vertexName){
-    pVertex newVertex = malloc(sizeof(struct ALIST_VERTEX));
+pHead addHead(pGraph graph, char* vertexName){
+    pHead newHead = malloc(sizeof(struct HEAD_VERTEX));
   
-    assert(strncpy(newVertex->name, vertexName, MAX_ID_LEN));
-    assert(newVertex->name[MAX_ID_LEN] = '\0');
-    newVertex->next = NULL;
+    assert(strcpy(newHead->name, vertexName));
+    // assert(newHead->name[strlen(newHead->name)-1] = '\0');
+    assert((newHead->nextHead = NULL)==0);
+    if(graph->heads == NULL) {
+        graph->heads = newHead;
+        printf("Head added");
+    }
 
-    return newVertex;
+    else{
+        pHead c = graph->heads;
+        while(c->nextHead!=NULL) c = c->nextHead;
+        
+        c->nextHead = newHead;
+    }
+    printf("%s:", newHead->name);
+    return newHead;
 }
 
+pGraph gcreate(int V){
+    pGraph graph = malloc(sizeof(struct UDGRAPH));
+    assert(graph->nV = V);
+    assert(graph->heads = malloc(V*sizeof(struct ALIST_VERTEX*)));
+
+    printf("Graph created");
+    return graph;
+}
+
+void addAdjacent(pHead head, char* neighbor){
+    pVertex newVertex = malloc(sizeof(struct ALIST_VERTEX));
+    assert(strcpy(newVertex->name, neighbor));
+    assert(newVertex->name[MAX_ID_LEN] = '\0');
+    
+    if(head->list == NULL) head->list = newVertex;
+    else{
+        pVertex c = head->list;
+        while(c->next!=NULL) c = c->next;
+        
+        c->next = newVertex;
+    }
+    printf("Adj created");
+}
+
+pHead findHead(pGraph GDS, char* name){
+    pHead current = GDS->heads;
+    int flag = 1;
+    while(strcmp(current->name,name)!=0 && flag){
+        current = current->nextHead;
+
+        if(current == NULL) {
+            flag=0;
+            printf("Head not found");
+        }
+    }
+
+    printf("%s", current->name);
+    return current;
+    
+    
+}
+
+void printGraph(pGraph graph){
+    pHead curhead = graph->heads;
+    pVertex curvertex = curhead->list;
+    while(curhead!=NULL){
+        printf("test");
+        printf("%s", curhead->name);
+        while(curvertex!=NULL){
+            printf("%s", curvertex->name);
+            curvertex = curvertex->next;
+        }
+        curhead = curhead->nextHead;
+    }
+}
+
+int main(){
+
+    pGraph graph = gcreate(4);
+    pHead head = findHead(graph, "Diana");
+
+    if(!head) head = addHead(graph, "Diana");
+    addAdjacent(head, "Kurt");
+
+    printGraph(graph);
+
+}
