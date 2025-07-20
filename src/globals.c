5d34d8c8a83be7b21f20fb9aa7e577a3c6937847
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-07-20 14:53:03 +0800

    add: add globals

---
+++ src/globals.c
@@ -0,0 +1,13 @@
+#ifndef GLOBAL
+#define GLOBAL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+#include "graph.c"
+
+#define MAX_ID_LEN 8
+
+#endif
